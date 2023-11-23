3db5bc879bf23cf91f7527de3271cc62fb3eb05a
IgnacioMurubeCrego <imurubec@alumnos.nebrija.es>
2023-11-23 09:20:44 +0100

    Correct main try/catch

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@
 import express from "express";
 import mongoose from "mongoose";
 
-const MONGO_URL = Deno.env.get("MONGO_URL");
+try {
+  const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
-  console.log("No mongo URL found");
-  throw new Error("MONGO_URL not defined.");
+  throw new Error("No mongo URL found");
 }
+
+
 
 await mongoose.connect(MONGO_URL);
 const app = express();
@@ -19,3 +21,7 @@ app
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
+} catch (error) {
+  console.log(error);
+}
